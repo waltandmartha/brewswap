f01106172c372c4eb5fb16f9ad2d9c5f1a0ef6d8
Glenn <waltandmartha@gmail.com>
2018-05-04 11:30:12 +1000

    sign in works

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable #omniauth_providers: [:google]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   resources :drinks
   get 'home/index'
-
+  root "home#index"
   resources :profiles
   resources :posts
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "home#index"
+
   
 
 end
